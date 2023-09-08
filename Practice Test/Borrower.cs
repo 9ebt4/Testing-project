51f22b2357363fdf192305c19cee659f569e1559
ethan <ethanbjansen@gmail.com>
2023-09-08 15:11:56 -0500

    Testing

---
+++ Practice Test/Borrower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Test
+{
+    public class Borrower
+    {
+        public string name { get; set; }
+        public int Dti { get; set; }
+        public int properties { get; set; }
+
+        public static string DivideNumber(string num1, string num2)
+        {
+            double numerator = 0;
+            double denominator = 0;
+
+            if (!double.TryParse(num1, out numerator) || !double.TryParse(num2, out denominator))
+            {
+                return "bad data";
+            }
+
+            try
+            {
+                return (numerator / denominator).ToString();
+            }
+            catch(DivideByZeroException) 
+            {
+                return "infinity";
+            }
+        }
+    }
+}
---
+++ Practice Test/Program.cs
@@ -0,0 +1,6 @@
+﻿using Practice_Test;
+
+Borrower borrower = new Borrower();
+borrower.name = "Ethan";
+borrower.Dti = 24;
+borrower.properties = 4;
